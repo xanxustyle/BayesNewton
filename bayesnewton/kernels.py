e22632bf9b735d447bb00b53324b9f865eab646f
Hui Sheng Lim <73118623+xanxustyle@users.noreply.github.com>
2022-09-09 13:11:43 +1000

    Update kernels.py

--- bayesnewton/kernels.py
+++ bayesnewton/kernels.py
@@ -359,6 +359,20 @@ class Matern72(StationaryKernel):
         return F
 
 
+class SquaredExponential(StationaryKernel):
+    """
+    The radial basis function (RBF) or squared exponential kernel. The kernel equation is
+        k(r) = σ² exp{-½ r²}
+    where:
+    r   is the Euclidean distance between the input points, scaled by the lengthscales parameter ℓ.
+    σ²  is the variance parameter
+    Functions drawn from a GP with this kernel are infinitely differentiable!
+    """
+
+    def K_r(self, r):
+        return self.variance * np.exp(-0.5 * np.square(r))
+
+
 class SpatioTemporalKernel(Kernel):
     """
     The Spatio-Temporal GP class
